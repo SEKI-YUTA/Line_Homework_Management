17dc6937d7eb84fd05c83e3b0556438ddc20876e
SEKI-YUTA <zard.since1991@gmail.com>
2022-04-24 00:29:05 +0900

    edit: edit db_client.js for heroku environment

--- db_client.js
+++ db_client.js
@@ -1,18 +1,20 @@
 const { Client } = require("pg");
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
 const client = new Client({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "homeworks",
-  password: "rootadmin",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+// for ngrok in local enviriroment
+// const client = new Client({
+//   user: "postgres",
+//   host: "127.0.0.1",
+//   database: "homeworks",
+//   password: "rootadmin",
+//   port: 5432,
+// });
 
 client.connect();
 exports.client = client;
