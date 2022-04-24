3739198171bb3401de2cf4e519176ca1d0536235
SEKI-YUTA <zard.since1991@gmail.com>
2022-04-24 10:18:28 +0900

    edit db_client.js

--- db_client.js
+++ db_client.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg");
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 // for ngrok in local enviriroment
 // const client = new Client({
@@ -17,13 +17,13 @@ const { Client } = require("pg");
 // });
 
 // for heroku environment
-const client = new Client({
-  user: "akvzqkcbuehbko",
-  host: "https://ec2-52-3-60-53.compute-1.amazonaws.com",
-  database: "d6r39sj0frn0gd",
-  password: "a1fc263c9ebd5b190c0dfc5927a5b6203ea0a864259894a048534e1dece50492",
-  port: 5432,
-});
+// const client = new Client({
+//   user: "akvzqkcbuehbko",
+//   host: "ec2-52-3-60-53.compute-1.amazonaws.com",
+//   database: "d6r39sj0frn0gd",
+//   password: "a1fc263c9ebd5b190c0dfc5927a5b6203ea0a864259894a048534e1dece50492",
+//   port: 5432,
+// });
 
 client.connect();
 exports.client = client;
